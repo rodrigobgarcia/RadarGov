b2b8004d3778ba2f8f63ba75bdcd15a98b3817f8
Roddy192 <rodrigo.bgarcia@hotmail.com>
2025-09-07 23:18:38 -0300

    Commit - 07/09/25 - Rodrigo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <div class="flex-col ">
+        <div>LOGO</div>
+        <div>MENU</div>
+        <div>
+            <div>sino</div>
+            <div>GET STARTED</div>
+        </div>
+    </div>
+</template>
