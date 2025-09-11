461ac8b6172008b1e17b3327115eddd3347571b8
Roddy192 <rodrigo.bgarcia@hotmail.com>
2025-09-11 16:42:16 -0300

    Commit 11-09-25 - Rodrigo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,15 @@
 <template>
 
-    <div class="flex justify-between items-center w-full fixed top-0 left-0 border border-black bg-white/30 font-extrabold h-20">
+    <div class="flex justify-between items-center w-full fixed top-0 left-0 border border-black bg-[#f1fcfc] font-extrabold h-20 text-lg">
         <div class="ml-5">LOGO</div>
         <div>MENU</div>
         <div class="flex justify-between w-40 mr-5">
-            <div class="font-bold">sino</div>
-            <div>GET STARTED</div>
+            <div class="font-bold">
+                <span class="material-symbols-outlined">notifications</span>
+            </div>
+            <div>
+                GET STARTED
+            </div>
         </div>
     </div>
 </template>
