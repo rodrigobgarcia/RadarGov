62500d5cf8cb974ca52ee4510455dd0e507088a1
Roddy192 <rodrigo.bgarcia@hotmail.com>
2025-09-12 15:45:33 -0300

    Commit - 12/09/25 - Rodrigo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,22 @@
 <template>
 
-    <div class="flex justify-between items-center w-full fixed top-0 left-0 border border-black bg-[#f1fcfc] font-extrabold h-20 text-lg">
-        <div class="ml-5">LOGO</div>
-        <div>MENU</div>
-        <div class="flex justify-between w-40 mr-5">
+    <div class="flex justify-between items-center w-full fixed top-0 left-0 border border-black bg-[#f1fcfc] font-extrabold h-32 text-xl font-sans">
+        <div class="ml-5 flex items-center gap-2">
+            <div><img src="../assets/radargov-high-resolution-logo.png" alt="" class="size-25"></div>
+            <div >RADARGOV</div>
+        </div>
+        <div class="flex gap-8 font-medium">
+            <div>Dashboard</div>
+            <div>Oportunidades</div>
+            <div>Analytics</div>
+            <div>Opções</div>
+        </div>
+        <div class="flex gap-8 w-55 mr-5 items-center">
             <div class="font-bold">
                 <span class="material-symbols-outlined">notifications</span>
             </div>
-            <div>
-                GET STARTED
+            <div class="bg-[#154c79] rounded-xl w-40 h-12 text-white flex items-center justify-center ">
+                <div>GET STARTED</div> 
             </div>
         </div>
     </div>
