0c1101a5630b7dbc7a102c9e3309699e3a7380b7
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-09-22 23:21:07 +0000

    Começo simples no front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
- <div class="flex justify-center items-center w-full h-[600px] border border-black">
-    <div class="flex flex-col items-center text-center gap-4">
-      
-      <div class="bg-[#daa922] w-1/4">
+ <div class="flex justify-center items-center w-full h-[600px] border border-black flex flex-col">
+    <div class="flex flex-col items-center text-center gap-4 mb-8">
+
+      <div class="bg-[#daa922] text-white px-2 rounded-md py-0.5 font-semibold">
         <div>Inteligência potencializada por IA</div>
       </div>
 
@@ -19,6 +19,10 @@
         busca e sugere oportunidades de Licitação relevantes para o seu negócio
       </div>
 
+    </div>
+    <div class="flex flex-row gap-4">
+      <button class="text-xl font-medium bg-cyan-800 text-white py-1 px-4 rounded-md">Star Free Trial</button>
+      <button class="text-xl font-medium bg-white text-black py-1 px-4 rounded-md border border-gray-400">Watch Demo</button>
     </div>
   </div>
 </template>
