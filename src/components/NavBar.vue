ee2ab780fd1b1bfb6b32e6d37ba47d1386a56267
Roddy192 <rodrigo.bgarcia@hotmail.com>
2025-09-22 19:53:42 -0300

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="flex justify-between items-center w-full fixed top-0 left-0 border border-black bg-[#f1fcfc] font-extrabold h-32 text-xl font-sans">
+    <div class="flex justify-between items-center w-full fixed top-0 left-0 border border-black bg-[#f1fcfc]/50 font-extrabold h-32 text-xl font-sans">
         <div class="ml-5 flex items-center gap-2">
             <div><img src="../assets/radargov-high-resolution-logo.png" alt="" class="size-25"></div>
             <div >RADARGOV</div>
