d9960def812d64052429dbd64b998c8a67cef6e9
Roddy192 <rodrigo.bgarcia@hotmail.com>
2025-09-22 21:09:52 -0300

    commit - Rodrigo - 22-09-25

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Opportunities from '@/views/Opportunities.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/oportunidades',
+      name: 'oportunidades',
+      component: Opportunities
     },
     {
       path: '/about',
---
+++ src/views/Opportunities.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex justify-center items-center w-full h-[400px] border border-black">
+        <div class="flex flex-col items-center text-center gap-4 w-full">
+            <div class="font-sans font-semibold text-xl border border-black">Encontre suas oportunidades:</div>
+            <input type="text" placeholder="Digite algumas palavras para iniciar a busca" class="bg-gray-100 w-1/2 border border-black">
+            <div class="w-1/5 border border-black">
+                <button class="bg-gray-100 rounded-md w-1/3 ">Buscar</button>
+            </div>
+        </div>
+    </div>
+    <div class="justify-center w-full h-[1300px] border border-black">
+        <div class="flex justify-center items-center font-sans font-semibold text-2xl border border-black w-full h-1/6">
+            <p>Sugestões baseadas no seu perfil:</p>
+        </div>
+        <div class="grid grid-cols-3 gap-12 p-12 w-full h-full min-w-0 overflow-hidden">
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+            <div><img src="https://placehold.co/300" alt=""class="w-auto h-auto object-contain"></div>
+        </div>
+        
+    </div>
+</template>
