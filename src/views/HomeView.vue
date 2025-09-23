211a9dddb63e326b744d4f3b933c5082bd58f68c
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-09-23 00:08:36 +0000

    Mais algum front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,4 +25,56 @@
       <button class="text-xl font-normal bg-white text-black py-1 px-4 rounded-md border border-gray-400">Watch Demo</button>
     </div>
   </div>
+  <section class="py-16 px-4">
+        <div class="container mx-auto">
+            <div class="text-center mb-12">
+                <h2 class="mb-4 text-3xl font-bold text-gray-600">Everything You Need to Win More Bids</h2>
+                <p class="text-lg max-w-2xl mx-auto text-gray-500">Our comprehensive platform combines AI intelligence with intuitive design to streamline your bidding process.</p>
+            </div>
+        </div>
+        <div class="grid grid-cols-3 gap-6">
+          <div class="bg-emerald-50 rounded-lg shadow-md">Teste
+            <div class="flex flex-col">
+              <div>Icon</div>
+              <div>Title</div>
+              <div>Description</div>
+            </div>
+          </div>
+          <div class="bg-emerald-50 rounded-lg shadow-md">Teste
+            <div class="flex flex-col">
+              <div>Icon</div>
+              <div>Title</div>
+              <div>Description</div>
+            </div>
+          </div>
+          <div class="bg-emerald-50 rounded-lg shadow-md">Teste
+            <div class="flex flex-col">
+              <div>Icon</div>
+              <div>Title</div>
+              <div>Description</div>
+            </div>
+          </div>
+          <div class="bg-emerald-50 rounded-lg shadow-md">Teste
+            <div class="flex flex-col">
+              <div>Icon</div>
+              <div>Title</div>
+              <div>Description</div>
+            </div>
+          </div>
+          <div class="bg-emerald-50 rounded-lg shadow-md">Teste
+            <div class="flex flex-col">
+              <div>Icon</div>
+              <div>Title</div>
+              <div>Description</div>
+            </div>
+          </div>
+          <div class="bg-emerald-50 rounded-lg shadow-md">Teste
+            <div class="flex flex-col">
+              <div>Icon</div>
+              <div>Title</div>
+              <div>Description</div>
+            </div>
+          </div>
+        </div>
+    </section>
 </template>
