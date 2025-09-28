c73ad11ba4e82e8691d5760c779d86f19e4a1c95
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-09-28 09:43:20 -0300

    fix: ajustes navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,12 @@
 <template>
 
-    <div class="flex justify-between items-center w-full fixed top-0 left-0 border border-black bg-[#f1fcfc]/50 font-extrabold h-32 text-xl font-sans">
-        <div class="ml-5 flex items-center gap-2">
-            <div><img src="../assets/radargov-high-resolution-logo.png" alt="" class="size-25"></div>
-            <div class="text-cyan-900 text-2xl">RADARGOV</div>
+    <div class="flex justify-between items-center w-full fixed top-0 left-0 bg-[#e6eef9]/95 h-16 text-xl font-sans px-[40px]">
+        <div class="flex items-center gap-2">
+            <!-- <div><img src="../assets/radargov-high-resolution-logo.png" alt="" class="size-25"></div>
+            <div class="text-cyan-900 text-2xl">RADARGOV</div> -->
+            <img src="../assets/logo.png" alt="Logo RadarGov" class="h-10">
         </div>
-        <div class="flex gap-8 font-extrabold text-gray-600 text-2xl">
+        <div class="flex gap-8 text-gray-600 text-xl">
             <div>Dashboard</div>
             <div>
                 <router-link to="/oportunidades" class="cursor-pointer hover:text-gray-300">
@@ -15,13 +16,13 @@
             <div>Analytics</div>
             <div>Opções</div>
         </div>
-        <div class="flex gap-8 w-55 mr-5 items-center">
-            <div class="font-bold">
+        <div class="flex gap-8  items-center">
+            <div class="cursor-pointer hover:text-gray-300">
                 <span class="material-symbols-outlined">notifications</span>
             </div>
-            <div class="bg-cyan-900 rounded-lg px-3 py-2 text-white font-medium flex items-center justify-center ">
-                <div>Get Started</div> 
-            </div>
+            <button class="bg-[#253b7f] rounded-lg px-3 py-2 text-white text-[16px] flex items-center justify-center max-h-[34px]">
+                Get Started
+            </button>
         </div>
     </div>
     
