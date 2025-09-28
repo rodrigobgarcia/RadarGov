e4b043a5f25a013da4455f998657dc0245aae255
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-09-28 18:32:53 -0300

    chore: ajustes deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/RadarGov/',  // <== ADICIONE ESTA LINHA
   plugins: [
     vue(),
     vueDevTools(),
