5840dee482228b5dff9479bd76157536d620d764
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-10-13 20:43:13 -0300

    fix: corrigir program git

--- RadarGov.Back/RadarGov/RadarGov.API/Program.cs
+++ RadarGov.Back/RadarGov/RadarGov.API/Program.cs
@@ -1,3 +1,5 @@
+using Quartz;
+using RadarGov.API.Jobs;
 namespace RadarGov.API
 {
     public class Program
@@ -10,6 +12,22 @@ namespace RadarGov.API
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            builder.Services.AddQuartz(q =>
+            {
+
+                var jobKey = new JobKey("JobGetLicitacoes");
+
+                q.AddJob<JobGetLicitacoes>(opts => opts.WithIdentity(jobKey));
+
+                q.AddTrigger(opts => opts.ForJob(jobKey)
+                .WithIdentity("JobGetLicitacoes-trigger")
+                .WithCronSchedule("0/1 * * * * ?"));
+
+            });
+
+            builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
