6ea10f06b3ae979f7d86a4d1fd3bd0b0c50d15b7
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-10-16 21:42:23 -0300

    Add Poder
    Entidade
    Service (Incompleta)
    Dto

---
+++ RadarGov.Back/RadarGov/RadarGov.Dominio/Servicos/PoderServico.cs
@@ -0,0 +1,23 @@
+﻿using RadarGov.Dominio.Entidades;
+using RadarGov.Dominio.Interfaces;
+using RadarGov.Integracoes.Pnc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarGov.Dominio.Servicos
+{
+    public class PoderServico
+    {
+        private readonly IImportacaoTerceiroRepositorio<Poder> _poderRepositorio;
+        private readonly Pncp _pncp;
+
+        public PoderServico(IImportacaoTerceiroRepositorio<Poder> poderRepositorio)
+        {
+            _poderRepositorio = poderRepositorio;
+            _pncp = new Pncp();
+        }
+    }
+}
