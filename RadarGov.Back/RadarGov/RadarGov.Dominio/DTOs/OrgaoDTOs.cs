4b960ca82de8049437bdcbd014074c39cb32d8c8
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-10-16 21:07:55 -0300

    adicionado Orgao Entidade e DTO

---
+++ RadarGov.Back/RadarGov/RadarGov.Dominio/DTOs/OrgaoDTOs.cs
@@ -0,0 +1,23 @@
+﻿using RadarGov.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarGov.Dominio.DTOs
+{
+    public class OrgaoDTOs
+    {
+        public class FiltrosDto
+        {
+            public List<Orgao> Orgaos { get; set; } = new();
+        }
+    }
+
+    public class OrgaoDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RadarGov.Back/RadarGov/RadarGov.Dominio/Entidades/Orgao.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RadarGov.Dominio.Entidades
+{
+    public class Orgao : BaseImportacaoTerceiro
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cnpj { get; set; }
+
+        public Orgao(string idTerceiro, string nome, string cnpj) 
+        {
+            this.IdTerceiro = idTerceiro;
+            this.Nome = nome;
+            this.Cnpj = cnpj;
+            this.CriadoEm = DateTime.Now;
+            this.UltimaAlteracao = DateTime.Now;
+        }
+
+        private Orgao()
+        {
+
+        }
+    }
+}
