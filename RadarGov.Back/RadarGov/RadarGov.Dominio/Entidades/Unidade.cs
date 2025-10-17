6dadcf7e8fb43326683e580bf0c7cee36660e7b5
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-10-16 21:29:27 -0300

    adicionado Unidade arquivos
    Add Unidade Service(Incompleta), DTO e Entidade

---
+++ RadarGov.Back/RadarGov/RadarGov.Dominio/Entidades/Unidade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarGov.Dominio.Entidades
+{
+    public class Unidade : BaseImportacaoTerceiro
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Id { get; set; } 
+
+        public string Nome { get; set; }    
+
+        public int Codigo { get; set; }
+
+        public string CodigoNome { get; set; }
+
+        public Unidade(string idTerceiro, string nome, int codigo, string codigoNome)
+        {
+            this.IdTerceiro = idTerceiro;
+            this.Nome = nome;
+            this.Codigo = codigo;
+            this.CodigoNome = codigoNome;
+            this.CriadoEm = DateTime.Now;
+            this.UltimaAlteracao = DateTime.Now;
+        }
+
+        private Unidade()
+        {
+
+        }
+    }
+}
---
+++ RadarGov.Back/RadarGov/RadarGov.Dominio/Servicos/UnidadeServico.cs
@@ -0,0 +1,23 @@
+﻿using RadarGov.Dominio.Entidades;
+using RadarGov.Dominio.Interfaces;
+using RadarGov.Integracoes.Pnc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarGov.Dominio.Servicos
+{
+    public class UnidadeServico
+    {
+        private readonly IImportacaoTerceiroRepositorio<Unidade> _unidadeRepositorio;
+        private readonly Pncp _pncp;
+
+        public UnidadeServico(IImportacaoTerceiroRepositorio<Unidade> unidadeRepositorio)
+        {
+            _unidadeRepositorio = unidadeRepositorio;
+            _pncp = new Pncp();
+        }
+    }
+}
