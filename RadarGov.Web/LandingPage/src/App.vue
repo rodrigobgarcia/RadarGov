9e6e2d4ebe7edd9d0509ba570fe808c0f64174df
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-10-27 20:39:31 -0300

    fix: ajustes app.vue

--- RadarGov.Web/LandingPage/src/App.vue
+++ RadarGov.Web/LandingPage/src/App.vue
@@ -1,18 +1,22 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue';
+import { RouterView, useRoute } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+import SideMenu from './components/SideMenu.vue'
 
-
+const route = useRoute()
 </script>
 
 <template>
-<NavBar></NavBar>
+    <NavBar v-if="route.path === '/' && route.path !== '/login'" />
+    <SideMenu v-if="route.path !== '/' && route.path !== '/login'" />
 
-<main class="pt-16"> 
-    <RouterView />
-</main>
+    <main :class="route.path === '/' ? 'pt-16' : route.path !== '/login'?  'ml-[70px]' : ''">
+        <RouterView />
+    </main>
 </template>
 
 <style scoped>
-
+main {
+    min-height: 100vh;
+}
 </style>
