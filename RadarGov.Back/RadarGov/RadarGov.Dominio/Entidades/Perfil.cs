a8acc7291403b6b945e60d2c683b32ab4bdf1433
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-10-31 19:57:24 -0300

    feat: add perfis

---
+++ RadarGov.Back/RadarGov/RadarGov.Dominio/Entidades/Perfil.cs
@@ -0,0 +1,9 @@
+﻿namespace RadarGov.Dominio.Entidades
+{
+    public class Perfil
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public List<string>? Permissoes { get; set; }
+    }
+}
